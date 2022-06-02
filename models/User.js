23e1e44112dbeec7bc636665ec538ea2bd17b0b9
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-06-02 22:13:29 +0100

    adding user.js model

--- models/User.js
+++ models/User.js
@@ -1,48 +1,41 @@
 // Require schema and model from mongoose
 const mongoose = require('mongoose');
 
-// Construct a new instance of the schema class
-const bookSchema = new mongoose.Schema({
-    // Configure individual properties using Schema Types
-    title: { type: String, required: true },
-    author: { type: String, required: false },
-    // The type of data is set to 'String' and required is set to false, meaning it will accept null values
-    publisher: String,
-    stockCount: Number,
-    price: Number,
-    inStock: Boolean,
-    // Use built in date method to get current date
-    lastAccessed: { type: Date, default: Date.now },
-  });
-  
-  // Using mongoose.model() to compile a model based on the schema 'bookSchema'
-  const Book = mongoose.model('Book', bookSchema);
-  
-  const handleError = (err) => console.error(err);
-  
-  // Create a new instance of the model, a document
-  Book.create(
+//Configuring individual properties using Schema Types
+const userSchema = new mongoose.Schema(
     {
-      title: 'Diary of Anne Frank',
-      author: 'Anne Frank',
-      publisher: 'Scholastic',
-      stockCount: 10,
-      price: 10,
-      inStock: true,
+      username: { 
+        type: String,
+        required: true,
+        unique: true
+        trim: true
     },
-    (err) => (err ? handleError(err) : console.log('Created new document'))
+
+    email: { 
+        type: String, 
+        required: true,
+        unique: true,
+    },
+
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+    }],
+
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      }],
+
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
   );
-  
-  // Create a new instance with required title and optional author properties
-  Book.create(
-    { title: 'Oh the Places You Will Go!', author: 'Dr. Seuss' },
-    (err) => (err ? handleError(err) : console.log('Created new document'))
-  );
-  
-  // Create a new instance with only required title
-  Book.create({ title: 'Harold and the Purple Crayon' }, (err) =>
-    err ? handleError(err) : console.log('Created new document')
-  );
-  
-  module.exports = Book;
+
+
+  module.exports = User;
   
